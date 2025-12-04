85f7312ed7acb4e476b7b62bb91f1540147eb006
cfgarciaq <cfgarciaq@gmail.com>
2025-12-04 22:15:51 +0100

    Make Name and Phone properties required in Client class
    Updated the `Name` and `Phone` properties in the `Client`
    class to use the `required` keyword. This change enforces
    mandatory initialization of these properties during object
    creation, improving data integrity and preventing null or
    uninitialized values.

--- Models/Client.cs
+++ Models/Client.cs
@@ -3,7 +3,7 @@
     public class Client
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
+        public required string Name { get; set; }
+        public required string Phone { get; set; }
     }
 }
